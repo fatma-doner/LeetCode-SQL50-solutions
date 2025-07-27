fa2006769db7377a33361ad80728ac7f5b78509a
Fatma Doner <fatmadoner72@gmail.com>
2025-07-27 23:15:05 +0300

    Add solution for Problem 1484 - Group Sold Products By The Date

---
+++ Solution1484.sql
@@ -0,0 +1,12 @@
+SELECT 
+    TO_CHAR(sell_date, 'YYYY-MM-DD') AS sell_date, 
+    COUNT(DISTINCT product) AS num_sold, 
+    LISTAGG(product, ',') WITHIN GROUP (ORDER BY product) AS products
+FROM 
+    (SELECT 
+    DISTINCT sell_date, 
+    product FROM Activities)
+GROUP BY 
+    sell_date
+ORDER BY 
+    sell_date;
