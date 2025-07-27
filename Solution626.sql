20bd3fe88e3fe6f1f9ca808eb0a1317d476b1d59
Fatma Doner <fatmadoner72@gmail.com>
2025-07-27 23:45:06 +0300

    Add second solution for Problem 626 - Exchange Seats

--- Solution626.sql
+++ Solution626.sql
@@ -4,3 +4,16 @@ select id, ISNULL(
     student
 ) student 
 from Seat;
+
+
+-- second solution without using lead or lag
+
+SELECT id,
+    CASE 
+        WHEN id % 2 = 1 THEN 
+            (SELECT student FROM Seat WHERE id = s.id + 1)
+        ELSE 
+            (SELECT student FROM Seat WHERE id = s.id - 1)
+    END AS student
+FROM Seat s
+WHERE id % 2 = 1 OR id % 2 = 0;
